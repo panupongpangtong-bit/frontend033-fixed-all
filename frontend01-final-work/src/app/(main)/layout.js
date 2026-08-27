5e2a9f1ab44c13a4068e2c1c4755d9f38969e9ca
panupongpangtong-bit <panupongpangtong@gmail.com>
2026-08-27 23:03:30 +0700

    fix cart provider

--- frontend01-final-work/src/app/(main)/layout.js
+++ frontend01-final-work/src/app/(main)/layout.js
@@ -1,15 +1,16 @@
 import Navbar from "@/components/navigation";
 import { CartProvider } from "@/app/(main)/context/cartcontext";
+
 export default function MainLayout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
+    <CartProvider>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
 
-      <main className="flex-1">
-      <CartProvider>
-  {children}
-</CartProvider>
-      </main>
-    </div>
+        <main className="flex-1">
+          {children}
+        </main>
+      </div>
+    </CartProvider>
   );
 }
